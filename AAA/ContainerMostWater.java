0818c0489974a6b5b3b03d48da476f8b8b3b81fb
bkjames <sweden011@gmail.com>
2018-10-02 17:42:33 +0900

    Update ContainerMostWater.java

--- AAA/ContainerMostWater.java
+++ AAA/ContainerMostWater.java
@@ -1,33 +1,26 @@
-package aExe01;
+package zz03;
 
-public class ContainerMostWater {
-
-	
-	
+public class ContainMostWater {
 	
 	public static void main(String[] args) {
 		int[] nums = {1,8,6,2,5,4,8,3,7};
 		System.out.println(maxArea(nums));
-		
 	}
 	
 	public static int maxArea(int[] height) {
-		if (height == null || height.length < 2) {
-			return 0;
-		}
-	 
-		int max = 0;
-		int left = 0;
-		int right = height.length - 1;
-	 
-		while (left < right) {
-			max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
-			if (height[left] < height[right])
+		if(height== null || height.length<2) return 0;
+		
+		int max =0;
+		int left=0;
+		int right= height.length-1;
+		while(left<right) {
+			max = Math.max(max, (right-left) * Math.min(height[left], height[right]));
+			if(height[left]<height[right])
 				left++;
 			else
 				right--;
 		}
-	 
 		return max;
 	}
+
 }
