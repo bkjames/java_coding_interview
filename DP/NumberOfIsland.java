fda8dc2f1396f5a0cfd1977a331d2893f18eacf5
bkjames <sweden011@gmail.com>
2018-10-02 17:17:32 +0900

    Update NumberOfIsland.java

--- DP/NumberOfIsland.java
+++ DP/NumberOfIsland.java
@@ -1,68 +1,61 @@
-package aaa;
+package zz03;
 
-
-public class NumberOfIsland {
+public class NumberOfIslands {
+	
+	public static void main(String[] args) {
 		
-		 // Driver method
-	    public static void main (String[] args) throws java.lang.Exception
-	    {
-
-	                                
-//	     char M[][]=   {{'1', '1', '0', '0', '0'},
-//	                    {'1', '1', '0', '0', '1'},
-//	                    {'0', '0', '1', '1', '1'},
-//	                    {'0', '0', '0', '0', '0'},
-//	                    {'1', '0', '1', '0', '1'}
-//	                                   };
-	     
-	     char M[][]=   {
-	    		 {'1', '1', '0', '0', '0'},
-                 {'1', '1', '0', '0', '0'},
-                 {'0', '0', '1', '0', '0'},
-                 {'0', '0', '0', '1', '1'}
-                        };
-	        NumberOfIsland I = new NumberOfIsland();
-	        System.out.println("Number of islands is: "+ I.numIslands(M));
-	    }
-
-		
-		public int numIslands(char[][] grid) {
-		    if(grid==null || grid.length==0||grid[0].length==0)
-		        return 0;
-		 
-		    int m = grid.length;
-		    int n = grid[0].length;
-		 
-		    int count=0;
-		    for(int i=0; i<m; i++){
-		        for(int j=0; j<n; j++){
-		            if(grid[i][j]=='1'){
-		            	System.out.println("i: "+i+" j:"+j);
-		                count++;
-		                merge(grid, i, j);
-		            }
-		        }
-		    }
-		 
-		    return count;
-		}
-		 
-		public void merge(char[][] grid, int i, int j){
-		    int m=grid.length;
-		    int n=grid[0].length;
-		    System.out.println("merge m: "+i+" n:"+j);
-		    System.out.println("merge i: "+i+" j:"+j);
-		 
-		    if(i<0||i>=m||j<0||j>=n||grid[i][j]!='1')
-		        return;
-		 
-		    grid[i][j]='X';
-		    System.out.println("       X i: "+i+" j:"+j);
-		 
-		    merge(grid, i-1, j);
-		    merge(grid, i+1, j);
-		    merge(grid, i, j-1);
-		    merge(grid, i, j+1);
-		}
-
+		char[][]M = {
+				{'1','1','1','1','0'},
+				{'1','1','0','1','0'},
+				{'1','1','0','0','0'},
+				{'0','0','0','0','0'}
+		};
+		numberOfIsland(M);
+		print(M);
 	}
+	
+	
+	public static void print(char[][] T) {
+		int m = T.length;
+		int n = T[0].length;
+		for (int i = 0; i < m; i++){
+			for (int j = 0; j < n; j++) {
+				System.out.print(T[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static int numberOfIsland(char[][] grid) {
+		if(grid== null|| grid.length==0 || grid[0].length==0)
+			return 0;
+		
+		int m = grid.length;
+		int n = grid[0].length;
+		int count =0;
+		for(int i=0; i<m; i++) {
+			for(int j=0; j<n; j++) {
+				if(grid[i][j]=='1') {
+					count++;
+					merge(grid,i,j);
+				}
+			}
+		}
+		return count;
+	}
+	public static void merge(char[][] grid, int i , int j) {
+		int m = grid.length;
+		int n = grid[0].length;
+		
+		if(i<0|| i>=m||j<0|| j>=n||grid[i][j] !='1') return;
+		
+		grid[i][j]='X';
+		
+		merge(grid, i-1, j);
+		merge(grid, i+1, j);
+		merge(grid, i, j-1);
+		merge(grid, i, j+1);
+	}
+	
+
+}
