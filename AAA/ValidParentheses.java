bf7172d7567ef309445df1ab06e1daeb5d8d6e80
bkjames <sweden011@gmail.com>
2018-10-02 17:32:56 +0900

    Update ValidParentheses.java

--- AAA/ValidParentheses.java
+++ AAA/ValidParentheses.java
@@ -1,4 +1,4 @@
-package zz01;
+package zz03;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,8 +6,7 @@ import java.util.Stack;
 
 public class ValidParentheses {
 	public static void main(String[] args) {
-		String exp ="{[)";
-//		String exp = "{()}[]";
+		String exp = "{()}[]";
 		System.out.println(isValid(exp));
 	}
 	
@@ -19,38 +18,20 @@ public class ValidParentheses {
 		
 		Stack<Character> stack = new Stack<>();
 		
-		for(int i=0 ; i< str.length(); i++) {
+		for(int i=0; i< str.length(); i++) {
 			char curr = str.charAt(i);
 			if(map.keySet().contains(curr)) {
 				stack.push(curr);
 			}else if(map.values().contains(curr)) {
-				if(!stack.empty() && stack.peek()== curr) {
+				if(!stack.empty() && map.get(stack.peek())==curr) {
 					stack.pop();
 				}else {
 					return false;
 				}
-				
 			}
 		}
 		return stack.empty();
 		
 	}
 
-}
-
-
-
-public boolean isValid(String s) {
-	Stack<Character> stack = new Stack<Character>();
-	for (char c : s.toCharArray()) {
-		if (c == '(')
-			stack.push(')');
-		else if (c == '{')
-			stack.push('}');
-		else if (c == '[')
-			stack.push(']');
-		else if (stack.isEmpty() || stack.pop() != c)
-			return false;
-	}
-	return stack.isEmpty();
 }
