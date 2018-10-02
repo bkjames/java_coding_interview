2f60cf263a3f7a52faabecf45421418185139f6d
bkjames <sweden011@gmail.com>
2018-10-02 17:20:26 +0900

    Update ValidParentheses.java

--- AAA/ValidParentheses.java
+++ AAA/ValidParentheses.java
@@ -1,3 +1,43 @@
+package zz01;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+	public static void main(String[] args) {
+		String exp ="{[)";
+//		String exp = "{()}[]";
+		System.out.println(isValid(exp));
+	}
+	
+	public static boolean isValid(String str) {
+		Map<Character, Character> map = new HashMap<>();
+		map.put('{', '}');
+		map.put('[', ']');
+		map.put('(', ')');
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i=0 ; i< str.length(); i++) {
+			char curr = str.charAt(i);
+			if(map.keySet().contains(curr)) {
+				stack.push(curr);
+			}else if(map.values().contains(curr)) {
+				if(!stack.empty() && stack.peek()== curr) {
+					stack.pop();
+				}else {
+					return false;
+				}
+				
+			}
+		}
+		return stack.empty();
+		
+	}
+
+}
+
 
 
 public boolean isValid(String s) {
