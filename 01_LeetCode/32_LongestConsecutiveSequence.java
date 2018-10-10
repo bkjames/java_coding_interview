8c8e51f4937af20463034faad8ab8a7268a00766
bkjames <sweden011@gmail.com>
2018-10-10 23:16:50 +0900

    Create 32_LongestConsecutiveSequence.java

---
+++ 01_LeetCode/32_LongestConsecutiveSequence.java
@@ -0,0 +1,47 @@
+package aExe07;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+	
+	public static void main(String[] args) {
+		int[] nums = {100, 4, 200, 1, 3, 2};
+		System.out.println(longestConsecutive(nums));
+	}
+
+	public static int longestConsecutive(int[] num) {
+		// if array is empty, return 0
+		if (num.length == 0) {
+			return 0;
+		}
+	 
+		Set<Integer> set = new HashSet<Integer>();
+		int max = 1;
+	 
+		for (int e : num)
+			set.add(e);
+	 
+		for (int e : num) {
+			int left = e - 1;
+			int right = e + 1;
+			int count = 1;
+	 
+			while (set.contains(left)) {
+				count++;
+				set.remove(left);
+				left--;
+			}
+	 
+			while (set.contains(right)) {
+				count++;
+				set.remove(right);
+				right++;
+			}
+	 
+			max = Math.max(count, max);
+		}
+	 
+		return max;
+	}
+}
