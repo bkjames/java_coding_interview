5a0c29fa0c600d6808427371eb4f7d4a41bbc1fb
bkjames <sweden011@gmail.com>
2018-10-11 00:04:53 +0900

    Create 6_NumberOfIsland.java

---
+++ 01_LeetCode/6_NumberOfIsland.java
@@ -0,0 +1,60 @@
+package aExe07;
+
+public class NumberOfIsland {
+	
+	public static void main(String[] args) {
+		char[][] M  = {
+				{'1','1','1','0','1'},
+				{'1','1','0','0','0'},
+				{'1','1','0','0','1'},
+				{'0','0','0','0','1'}
+		};
+		numberOfIsland(M);
+		print(M);
+	}
+	
+	public static void print(char[][] grid) {
+		int m = grid.length;
+		int n = grid[0].length;
+		for(int i=0; i<m; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.println(grid[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+
+	
+	public static int numberOfIsland(char[][] grid) {
+		if(grid==null|| grid.length==0||grid[0].length==0)
+			return 0;
+		
+		int m = grid.length;
+		int n = grid[0].length;
+		int count =0;
+		for(int i=0; i<m; i++) {
+			for(int j=0; j<n; j++) {
+				if(grid[i][j] == '1') {
+					count++;
+					merge(grid,i,j);
+				}
+			}
+		}
+		return count;
+	}
+	
+	public static void merge(char[][] grid, int i, int j) {
+		int m = grid.length;
+		int n = grid[0].length;
+		if(i<0||i>=m|| j<0||j>=n||grid[i][j]!='1') return;
+		grid[i][j]='X';
+		
+		merge(grid, i-1, j);
+		merge(grid, i+1, j);
+		merge(grid, i, j-1);
+		merge(grid, i, j+1);
+	}
+	
+	
+}
