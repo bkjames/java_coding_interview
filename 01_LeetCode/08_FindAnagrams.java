e4929d0f5d24cfa25df747b19fb9c00a170813b6
bkjames <sweden011@gmail.com>
2018-10-15 20:39:33 +0900

    Update 08_FindAnagrams.java

--- 01_LeetCode/08_FindAnagrams.java
+++ 01_LeetCode/08_FindAnagrams.java
@@ -13,6 +13,42 @@ public class FindAnagrams {
 	        String pat = "ABCD";
 	        System.out.println(findAnagrams2(txt, pat));
 	    }
+	
+	
+	 public static List<Integer> findAnagrams(String s, String p) {
+	        List<Integer> rst = new ArrayList<>();
+	        if (s == null || s.length() == 0 || s.length() < p.length()) {
+	            return rst;
+	        }
+
+	        int[] map_p = new int[26];
+	        for (int i = 0; i < p.length(); i++) {
+	            map_p[p.charAt(i) - 'A']++;
+	        }
+
+	        for (int i = 0; i < s.length() - p.length()+1; i++) {
+	            int[] map_s = new int[26];
+	            for (int j = 0; j < p.length(); j++) {
+	            	System.out.println("i: "+i+" j: "+j+" i+j: "+(i+j));
+	            	System.out.println("s.charAt(i+j): "+s.charAt(i+j));
+	                map_s[s.charAt(i+j) - 'A']++;
+	            }
+	            System.out.println();
+	            if (isMatch(map_p, map_s)) {
+	                rst.add(i);
+	            }
+	        }
+	        return rst;
+	    }
+
+	    public static boolean isMatch(int[] arr1, int[] arr2) {
+	        for (int i = 0; i < arr1.length; i++) {
+	            if (arr1[i] != arr2[i]) {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
 	  
 	  public static List<Integer> findAnagrams2(String s, String p) {
 	        List<Integer> rst = new ArrayList<>();
