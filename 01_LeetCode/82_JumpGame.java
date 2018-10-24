80f33f5c180f3a06c926414ef408fc1e3a9c79c4
bkjames <sweden011@gmail.com>
2018-10-24 19:39:40 +0900

    Create 82_JumpGame.java

---
+++ 01_LeetCode/82_JumpGame.java
@@ -0,0 +1,33 @@
+package coding05;
+
+public class JumpGame {
+
+	public static void main(String[] args) {
+		int[] nums = {2,3,1,1,4};
+		System.out.println(canJump(nums));
+	}
+	
+	public static boolean canJump(int[] A) {
+	    if(A.length <= 1)
+	        return true;
+	 
+	    int max = A[0]; //max stands for the largest index that can be reached.
+	 
+	    for(int i=0; i<A.length; i++){
+	        //if not enough to go to next
+	        if(max <= i && A[i] == 0) 
+	            return false;
+	 
+	        //update max    
+	        if(i + A[i] > max){
+	            max = i + A[i];
+	        }
+	 
+	        //max is enough to reach the end
+	        if(max >= A.length-1) 
+	            return true;
+	    }
+	 
+	    return false;    
+	}
+}
