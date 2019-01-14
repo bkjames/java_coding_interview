e46a5c10fa1445d4ced87b7920962268a64aae70
bkjames <sweden011@gmail.com>
2019-01-14 21:20:06 +0900

    Update 07_ValidParentheses.java

--- 01_LeetCode/07_ValidParentheses.java
+++ 01_LeetCode/07_ValidParentheses.java
@@ -10,6 +10,21 @@ public class ValidParentheses {
 		System.out.println(isValid(exp));
 	}
 	
+	public boolean isValid(String s) {
+	Stack<Character> stack = new Stack<Character>();
+		for (char c : s.toCharArray()) {
+			if (c == '(')
+				stack.push(')');
+			else if (c == '{')
+				stack.push('}');
+			else if (c == '[')
+				stack.push(']');
+			else if (stack.isEmpty() || stack.pop() != c)
+				return false;
+		}
+	return stack.isEmpty();
+	}
+	
 	public static boolean isValid(String str) {
 		Map<Character, Character> map = new HashMap<>();
 		map.put('{', '}');
