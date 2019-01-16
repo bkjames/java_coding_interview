89a5a7c6f9fe26ea5156c46066ba557c3320cae1
bkjames <sweden011@gmail.com>
2019-01-16 23:23:48 +0900

    Update 03_LongestSubstringWithoutRepeat.java

--- 01_LeetCode/03_LongestSubstringWithoutRepeat.java
+++ 01_LeetCode/03_LongestSubstringWithoutRepeat.java
@@ -9,6 +9,25 @@ public class LongestSubstringWithoutRepeat {
 		System.out.println(longestSub(str));
 	}
 	
+	
+	
+	
+  public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max=0;
+        for (int i=0, j=0; i<s.length(); ++i){
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-j+1);
+        }
+        return max;
+    }
+	
+	
+	
 	public static int  longestSub(String s) {
 		if(s==null || s.length()==0) return 0;
 		
