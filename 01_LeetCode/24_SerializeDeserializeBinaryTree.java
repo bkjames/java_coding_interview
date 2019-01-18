29ca0df18d8391eff315258eed869e004a9afa55
bkjames <sweden011@gmail.com>
2019-01-18 14:34:25 +0900

    Update 24_SerializeDeserializeBinaryTree.java

--- 01_LeetCode/24_SerializeDeserializeBinaryTree.java
+++ 01_LeetCode/24_SerializeDeserializeBinaryTree.java
@@ -8,9 +8,50 @@ import zz04.SerializeDeserialize.TreeNode;
 import java.lang.*;
 import java.io.*;
 
+public class Codec {
+    private static final String spliter = ",";
+    private static final String NN = "X";
 
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
+    }
+
+    private void buildString(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append(NN).append(spliter);
+        } else {
+            sb.append(node.val).append(spliter);
+            buildString(node.left, sb);
+            buildString(node.right,sb);
+        }
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Deque<String> nodes = new LinkedList<>();
+        nodes.addAll(Arrays.asList(data.split(spliter)));
+        return buildTree(nodes);
+    }
+    
+    private TreeNode buildTree(Deque<String> nodes) {
+        String val = nodes.remove();
+        if (val.equals(NN)) return null;
+        else {
+            TreeNode node = new TreeNode(Integer.valueOf(val));
+            node.left = buildTree(nodes);
+            node.right = buildTree(nodes);
+            return node;
+        }
+    }
+}
 
 public class Solution {
+	
+	
+	
+	
     /**
      * This method will be invoked first, you should design your own algorithm
      * to serialize a binary tree which denote by a root node to a string which
