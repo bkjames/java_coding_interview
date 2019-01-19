a98741e4bf88fdeb7d81913cd7f102679830eb96
bkjames <sweden011@gmail.com>
2019-01-19 21:40:09 +0900

    Create A_105_FirstUniqueChar.java

---
+++ 01_LeetCode/A_105_FirstUniqueChar.java
@@ -0,0 +1,27 @@
+
+import java.util.HashMap;
+
+public class FirstUniqueChar {
+
+	public static void main(String[] args) {
+		String s = "leetcode";
+		System.out.println(firstUniqChar(s));
+	}
+	
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+        
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1) 
+                return i;
+        }
+        return -1;
+    }
+	}
