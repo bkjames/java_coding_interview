5777a5dbe58afffceaa2cc2d3e46ef36e068d3f5
bkjames <sweden011@gmail.com>
2019-01-22 17:56:56 +0900

    Update 32_LongestConsecutiveSequence.java

--- 01_LeetCode/32_LongestConsecutiveSequence.java
+++ 01_LeetCode/32_LongestConsecutiveSequence.java
@@ -9,6 +9,43 @@ public class LongestConsecutiveSequence {
 		int[] nums = {100, 4, 200, 1, 3, 2};
 		System.out.println(longestConsecutive(nums));
 	}
+	
+	
+public int longestConsecutive(int[] nums) {
+    if(nums.length==0) return 0;
+    //num -> length;
+    Map<Integer, Integer> map = new HashMap<>();
+    int max = 1;
+    for(int num:nums)
+    {
+        if(map.containsKey(num)) continue;
+        int left = 0, right = 0, len = 0;
+        if(map.containsKey(num-1)&&map.containsKey(num+1))
+        {
+            left = num - map.get(num-1);
+            right = num + map.get(num+1);
+            len = right - left + 1;
+            map.put(left, len);
+            map.put(right, len);
+            map.put(num, 1);
+        }else if(map.containsKey(num-1))
+        {
+            left = num - map.get(num-1);
+            len = map.get(num-1) + 1;
+            map.put(left, len);
+            map.put(num, len);
+        }else if(map.containsKey(num+1))
+        {
+            right = num + map.get(num+1);
+            len = map.get(num+1) + 1;
+            map.put(num, len);
+            map.put(right, len);
+        }else
+            map.put(num, 1);          
+        max = Math.max(max, len);
+    }
+    return max;        
+}
 
 	public static int longestConsecutive(int[] num) {
 		// if array is empty, return 0
