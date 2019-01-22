c67d387b790d928331f5200ef8f059f1d779a10f
bkjames <sweden011@gmail.com>
2019-01-22 23:57:20 +0900

    Update 08_LongestPalindromeSubstring.java

--- 01_LeetCode/08_LongestPalindromeSubstring.java
+++ 01_LeetCode/08_LongestPalindromeSubstring.java
@@ -1,45 +1,47 @@
 package zz01;
 
-public class LongestPalindromSubstring {
+public class LongestPalindrominSubstring {
 	
-	
-	public String longestPalindrome(String s) {
-	if (s.isEmpty()) {
-		return null;
-	}
+	public static void main(String args[])
+	{
+		System.out.println(longestPalindrome("bananas"));
+//		System.out.println(LPS("bbbb"));
+	}	
+		
+	public static String longestPalindrome(String s) {
+		if (s.isEmpty()) 
+			return null;
+		if (s.length() == 1) 
+			return s;
 
-	if (s.length() == 1) {
-		return s;
-	}
+		String longest = s.substring(0, 1);
+		for (int i = 0; i < s.length(); i++) {
 
-	String longest = s.substring(0, 1);
-	for (int i = 0; i < s.length(); i++) {
-
-		// get longest palindrome with center of i
+	// get longest palindrome with center of i
 		String tmp = helper(s, i, i);
 		if (tmp.length() > longest.length()) {
 			longest = tmp;
 		}
 
-		// get longest palindrome with center of i, i+1
+	// get longest palindrome with center of i, i+1
 		tmp = helper(s, i, i + 1);
-		    if (tmp.length() > longest.length()) {
-			longest = tmp;
-		    }
+	    	if (tmp.length() > longest.length()) {
+	    		longest = tmp;
+	    	}
 		}
-	return longest;}
-
-	public String helper(String s, int begin, int end) {
-
-	while (begin >= 0 && end <= s.length() - 1 && s.charAt(begin) == s.charAt(end)) {
-
-		begin--;
-
-		end++;
-
+		return longest;
 	}
 
-	 return s.substring(begin + 1, end);
+	public static String helper(String s, int begin, int end) {
+
+		while (begin >= 0 && end <= s.length() - 1 && s.charAt(begin) == s.charAt(end)) {
+
+			begin--;
+			end++;
+		}
+
+		return s.substring(begin + 1, end);
+	}
 }
 
   public static String LPS(String s) {
