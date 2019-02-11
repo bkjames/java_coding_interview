9ed2c57b96d5cc2afa393bce5ca8b45136dfe6d5
bkjames <sweden011@gmail.com>
2019-02-11 16:33:40 +0900

    Create Array2D.java

---
+++ 03_Basic/Array2D.java
@@ -0,0 +1,34 @@
+package aaa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Array2D {
+
+	public static void main(String[] args) {
+		List<List<Integer>> list = new ArrayList<>();
+		
+		for(int i=0; i<5; i++) {
+			List<Integer> integers = new ArrayList<Integer>();
+			for(int j=0; j<5; j++) {
+				integers.add(i);
+			}
+			list.add(integers);
+		}
+		
+		int[][] array =new int[list.size()][];
+		
+		for(int i=0 ; i< array.length ; i++) {
+			array[i] = new int[list.get(i).size()];
+		}
+		
+		for(int i=0; i<list.size(); i++) {
+			for(int j=0; j<list.get(i).size(); j++) {
+				array[i][j] = list.get(i).get(j);
+			}
+		}
+		System.out.println(Arrays.deepToString(array));
+		
+	}
+}
