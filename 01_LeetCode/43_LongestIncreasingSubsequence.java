fadad97f854b43b9597647ac8a2745bc7a2e9826
bkjames <sweden011@gmail.com>
2019-02-16 21:16:05 +0900

    Update 43_LongestIncreasingSubsequence.java

--- 01_LeetCode/43_LongestIncreasingSubsequence.java
+++ 01_LeetCode/43_LongestIncreasingSubsequence.java
@@ -9,7 +9,7 @@ public class LongestIncreasingSubsequence {
 		System.out.println(lengthOfLIS(nums));
 		System.out.println(lengthOfLIS2(nums));
 	}
-	public static int lengthOfLIS(int[] nums) {
+		public static int lengthOfLIS(int[] nums) {
 	    if(nums==null || nums.length==0)
 	        return 0;
 	 
@@ -18,19 +18,47 @@ public class LongestIncreasingSubsequence {
 	    for(int i=0; i<nums.length; i++){
 	        max[i]=1;
 	        for(int j=0; j<i;j++){
+	        	System.out.println(i+" "+j);
+	        	System.out.println("aa "+nums[i]+" "+nums[j]);
 	            if(nums[i]>nums[j]){
+	            	System.out.println("max "+nums[i]+" "+nums[j]);
+	            	System.out.println("max val "+Math.max(max[i], max[j]+1));
 	                max[i]=Math.max(max[i], max[j]+1);
 	            }
+	            
 	        }
 	    }
 	 
 	    int result = 0;
 	    for(int i=0; i<max.length; i++){
+	    	System.out.println("res: "+max[i]);
 	        if(max[i]>result)
 	            result = max[i];
 	    }
 	    return result;
 	}
+	
+	
+    public static int lengthOfLIS2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < dp.length; i++) {
+            int maxval = 0;
+            for (int j = 0; j < i; j++) {
+            	System.out.println(nums[i]+" "+nums[j]);
+                if (nums[i] > nums[j]) {
+                    maxval = Math.max(maxval, dp[j]);
+                }
+            }
+            dp[i] = maxval + 1;
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
 	
 	
 	public  static int  lengthOfLIS2(int[] nums) {
