6ed7c4f14140f7d211d508d7d77b1dafb78ddefc
bkjames <sweden011@gmail.com>
2019-02-20 17:46:14 +0900

    Update 46_DiameterOfBinaryTree.java

--- 01_LeetCode/46_DiameterOfBinaryTree.java
+++ 01_LeetCode/46_DiameterOfBinaryTree.java
@@ -21,6 +21,56 @@ public class DiameterOfBinaryTree {
 	}
 	
 	
+	package easy;
+
+class TreeNode{
+	int val;
+	TreeNode left, right;
+	TreeNode(int x){
+		this.val = x;
+	}
+}
+
+public class DiameterOfBinaryTree {
+	
+	public static void main(String[] args) {
+		TreeNode node = new TreeNode(1);
+		node.left = new TreeNode(2);
+		node.right = new TreeNode(3);
+		node.left.left = new TreeNode(4);
+		node.left.right = new TreeNode(5);
+		DiameterOfBinaryTree a = new DiameterOfBinaryTree();
+		System.out.println("val: "+a.diameter(node));
+	}
+	
+	int ans ;
+	public  int diameter(TreeNode root) {
+		ans =1;
+		depth(root);
+		System.out.println("==========");
+		return ans-1;
+	}
+	
+	public  int depth(TreeNode node) {
+		if(node== null) return 0;
+		System.out.println(node.val);
+		
+//		if(node.left!= null)System.out.println("node.left: "+node.left.val);
+		int L = depth(node.left);
+		System.out.println("L: "+L);
+//		if(node.right!= null)System.out.println("node.right: "+node.right.val);
+//		System.out.println("node.right: "+node.right.val);
+		int R = depth(node.right);
+		System.out.println("R: "+R);
+		ans = Math.max(ans, L+R+1);
+		System.out.println("===end===="+node.val+" L: "+L+" R:"+R);
+		return Math.max(L, R)+1;
+	}
+
+}
+
+	
+	
 	public int getHeight(TreeNode root) {
 		if (root != null) {
 			return 1 + Math.max(getHeight(root.left), getHeight(root.right));
