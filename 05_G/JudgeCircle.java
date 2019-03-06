291981f19db4046e442447e868d5cd18f0761541
bkjames <sweden011@gmail.com>
2019-03-06 10:11:01 +0900

    Create JudgeCircle.java

---
+++ 05_G/JudgeCircle.java
@@ -0,0 +1,22 @@
+
+
+public class JudgeCircle {
+
+	
+	public static void main(String[] args) {
+		String s ="UD";
+		judgeCircle(s);
+		
+	}
+	
+	 public static boolean judgeCircle(String moves) {
+	        int x = 0, y = 0;
+	        for (char c : moves.toCharArray()) {
+	            if (c == 'R') x++;
+	            else if (c == 'L') x--;
+	            else if (c == 'U') y--;
+	            else if (c == 'D') y++;
+	        }
+	        return x == 0 && y == 0;
+	    }
+}
