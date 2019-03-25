1b7a9b66bd6a02ae92043a7b9f7f526d0fc6e9bc
bkjames <sweden011@gmail.com>
2019-03-25 16:16:56 +0900

    Update zBigNumberSoring.java

--- 07_ka/zBigNumberSoring.java
+++ 07_ka/zBigNumberSoring.java
@@ -1,21 +1,38 @@
 package prog;
 
-public class BigNumberSoring {
+public class BigNumberSorting {
 
-	public static String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++)
-            arr[i] = String.valueOf(numbers[i]);
- 
-                Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
- 
- 
-        if(arr[0].equals("0")) return "0";
-         
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++)
-            sb.append(arr[i]);
- 
-        return sb.toString();
-    }
-}
+	public static void main(String[] args) {
+		BigNumberSorting a = new BigNumberSorting();
+		int[] numbers =  {6, 10, 2};
+		System.out.println(a.solution(numbers));
+	}
+	
+	public String solution(int[] numbers) {
+		String answer ="";
+		String[] nums = new String[numbers.length];
+		for(int i=0; i< nums.length; i++) {
+			nums[i] = String.valueOf(numbers[i]);
+		}
+		
+		Arrays.sort(nums, Comp);
+		if(nums[0].equals("0")) {
+			answer += "0";
+		}else {
+			for(int j=0; j<nums.length; j++) {
+				answer +=nums[j];
+			}
+		}
+		return answer;
+	}
+	
+	Comparator<String> Comp = new Comparator<String>() {
+
+		@Override
+		public int compare(String a, String b) {
+			// TODO Auto-generated method stub
+			System.out.println((b+a)+"   "+(a+b)+" : "+((b+a).compareTo(a+b)));
+			return (b+a).compareTo(a+b);
+		}
+		
+	};
