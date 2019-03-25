d3fe5f885e7e0b242c85bc191b3ee6f7d94ec2e0
bkjames <sweden011@gmail.com>
2019-03-25 14:03:42 +0900

    Create TargetNumber_dfs.java

---
+++ 07_ka/TargetNumber_dfs.java
@@ -0,0 +1,152 @@
+package test0325;
+
+import java.util.*;
+
+//class Tree{
+class Node{
+    Node parent;
+    int data;
+    Node left;
+    Node right;
+    int index;
+    Node(int data, int index){
+        this.data = data;
+        this.index = index;
+    }
+}
+
+
+public class TargetNumber_dfs {
+
+//	public int solution(int[] numbers, int target) {
+//        return DFS(numbers, target, 0, 0);
+//    }
+// 
+//    public int DFS(int[] numbers, int target, int index, int num) {
+//        if(index == numbers.length) {
+//            return num == target ? 1 : 0;
+//        } else {
+//            return DFS(numbers, target, index + 1, num + numbers[index])
+//                    + DFS(numbers, target, index + 1, num - numbers[index]);
+//        }
+//    }
+ 
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+            System.out.print("arr["+i+"]"+ arr[i]+" ");
+        }
+        System.out.println();
+        int target = 3;
+ 
+        System.out.println(new TargetNumber_dfs().solution(arr, target));
+    }
+    
+//    1 queue bfs
+    public int solution(int[] numbers, int target) {
+        Queue<Node> Q = new LinkedList<Node>();
+        int answer = 0;
+        Q.add(new Node(0, 0));
+        while(!Q.isEmpty()){
+            Node a = Q.poll();
+            System.out.println("index:"+a.index + "data:" + a.data);
+            if(a.index==numbers.length){
+                if(a.data==target)
+                    answer++;
+                continue;
+            }
+
+            a.left = new Node(a.data + numbers[a.index] , a.index+1);
+            a.right = new Node(a.data - numbers[a.index] , a.index+1);
+            Q.add(a.left);
+            Q.add(a.right);
+        }
+        return answer;
+    }
+}
+
+
+    
+    
+  //음수 아닌 정수, +-로 수 만들기
+    //만드는 방법의 수
+    static int[] num;
+    static int len;
+    static int count = 0;
+    //경우의 수는 +/-
+    //targer = 목표 수 / tempNum = 현재 수 / i = 현재 index
+    public void check(int target, int tempNum, int i){
+        //i가 끝이면
+        if(i >= len - 1){
+            if(tempNum + num[i] == target){
+                count++;
+            }
+            if(tempNum - num[i] == target){
+                count++;
+            }  
+            return;
+        }
+        //i가 끝이 아니면 경우의 수 따지기
+        check(target, tempNum + num[i], i + 1);
+        check(target, tempNum - num[i], i + 1);
+    }
+    public int solution(int[] numbers, int target) {
+        len = numbers.length;
+        num = new int[len];
+        for(int i = 0; i < len; i++){
+            num[i] = numbers[i];
+        }
+        check(target, 0, 0);
+        return count;
+    }
+    
+    
+    2 dfs
+    private int count, size, t;
+    public int solution(int[] numbers, int target) {
+        size = numbers.length;
+        t = target;
+        dfs(0, numbers, 0);
+
+        return count;
+    }
+
+    private void dfs(int currentIndex, int[] numbers, int currentSum) {
+        if(currentIndex == size) {
+            if(currentSum == t) {
+                count++;
+            }
+            return;
+        }
+
+        dfs(currentIndex+1, numbers, currentSum+numbers[currentIndex]);
+        dfs(currentIndex+1, numbers, currentSum-numbers[currentIndex]);
+    }
+    
+    
+    
+    Queue<Integer> q = new LinkedList<Integer>();
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        q.offer(0);
+        q.offer(0);
+        while(q.peek()!=null){
+            int level =q.poll();
+            int v = q.poll();
+            if(level==numbers.length){
+                if(v==target)
+                    answer++;
+            }else {
+
+                q.offer(level + 1);
+                q.offer(v + numbers[level]);
+
+                q.offer(level + 1);
+                q.offer(v - numbers[level]);
+            }
+        }
+        return answer;
+    }
+//}
+
