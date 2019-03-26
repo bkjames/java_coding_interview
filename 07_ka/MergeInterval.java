c980d386dd48f8e31ce81bc163755418d14c6a48
bkjames <sweden011@gmail.com>
2019-03-26 11:23:46 +0900

    Update MergeInterval.java

--- 07_ka/MergeInterval.java
+++ 07_ka/MergeInterval.java
@@ -1,71 +1,82 @@
-package Test;
+package test0322;
 
 import java.util.*;
 
-class Interval {
-      int start;
-      int end;
-      Interval() { start = 0; end = 0; }
-      Interval(int s, int e) { start = s; end = e; }
- }
- 
 
 public class MergeInterval {
 	
+	static class Interval{
+		int start, end;
+		Interval(){
+			this.start=0;
+			this.end =0;
+		}
+		Interval(int s, int e){
+			this.start = s;
+			this.end = e;
+		}
+	}
+
+	
 	public static void main(String[] args) {
+		int[][] nums = {{1,3},{2,6},{8,10},{15,18}};
 		
-//		int[][] nums = {{1,3},{2,6},{8,10},{15,18}};
-		Interval in1 = new Interval(1,3);
+		Interval in = new Interval(1,3);
 		Interval in2 = new Interval(2,6);
 		Interval in3 = new Interval(8,10);
 		Interval in4 = new Interval(15,18);
 		
-		List<Interval> intervals = new ArrayList<>();
-		intervals.add(in1);
-		intervals.add(in2);
-		intervals.add(in3);
-		intervals.add(in4);
+		List<Interval> list = new ArrayList<>();
+		list.add(in);
+		list.add(in2);
+		list.add(in3);
+		list.add(in4);
+		
 		MergeInterval a = new MergeInterval();
-		List<Interval> list = a.merge(intervals);
-		a.print(list);
+		a.merge(list);
 	}
-	void print(List<Interval> list) {
-		for(int i=0 ; i<list.size(); i++ ) {
-			Interval in = list.get(i);
-			System.out.println(in.start+" "+in.end);
-		}
-	}
-
-	Comparator comp =  new Comparator<Interval>() {
-		public int compare(Interval a, Interval b) {
+	 public List<Interval> merge(List<Interval> intervals){
+		 List<Interval> resList  = new ArrayList<>();
+		 if(intervals.size() < 2) return intervals;
+		 print(intervals);
+		 
+		 Collections.sort(intervals, comp);
+		 
+		 Interval hold = intervals.get(0);
+		 for(int i=1; i< intervals.size(); i++) {
+			 Interval curr = intervals.get(i);
+			 System.out.println("current start end "+curr.start+"  "+curr.end);
+			 if(hold.end >= curr.start) {
+				 System.out.println("for "+hold.end+"  "+curr.start);
+				 hold.end = Math.max(hold.end, curr.end);
+				 System.out.println("curr.end "+hold.end);
+			 }else {
+				 resList.add(hold);
+				 hold = curr;
+			 } 
+		 }
+		 resList.add(hold);
+		 System.out.println("===sort ===");
+		 print(resList);
+		 return resList;
+		 
+	 }
+	 
+	 public void print(List<Interval> list) {
+		 int size = list.size();
+		 for(int i =0; i<size; i++) {
+			 Interval interval = list.get(i);
+			 System.out.println(interval.start +" "+ interval.end);
+		 }
+	 }
+	 
+	 Comparator<Interval> comp = new Comparator<Interval>() {
+		@Override
+		public int compare(Interval o1, Interval o2) {
 			// TODO Auto-generated method stub
-			return a.start - b.start;
+			System.out.println("o1.start: "+o1.start+" o2.start "+o2.start+"  o1.start- o2.start: "+ (o1.start- o2.start));
+			return o1.start- o2.start;
 		}
-	};
-	
-	public List<Interval> merge(List<Interval> intervals) {
-		if (intervals.isEmpty())
-			return intervals;
+	 };
 
-		Collections.sort(intervals,comp);
-		
-		
-
-		List<Interval> ans = new LinkedList<Interval>();
-		Interval hold = intervals.get(0);
-		for (int i = 1; i < intervals.size(); i++) {
-			Interval current = intervals.get(i);
-			if (hold.end >= current.start) {
-				hold.end = Math.max(current.end, hold.end);
-			} else {
-				ans.add(hold);
-				hold = current;
-			}
-		}
-
-		if (!ans.contains(hold))
-			ans.add(hold);
-
-		return ans;
-	}
 }
