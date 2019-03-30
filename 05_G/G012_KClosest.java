f224cd0ea41950629d20feb24511e869be658760
bkjames <sweden011@gmail.com>
2019-03-30 13:31:50 +0900

    Update G012_KClosest.java

--- 05_G/G012_KClosest.java
+++ 05_G/G012_KClosest.java
@@ -47,3 +47,67 @@ public class G012_KClosest {
 //	        return res;
 //	    }
 }
+
+import java.util.*;
+
+public class KClosest {
+
+	public static void main(String[] args) {
+		int[][] points = {{1,3},{-2,2}};
+		int K =1;
+		KClosest a = new KClosest();
+//		int[][] result = a.kClosest(points, k);
+		
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		for(int i=0; i<points.length; i++) {
+			List<Integer> sList= new ArrayList<Integer>();
+			for(int j=0; j<points[i].length; j++) {
+				sList.add(points[i][j]);
+			}
+			list.add(sList);
+		}
+		a.kClosest(list, K);
+//		a.print(points);
+	}
+	
+	public int[][] kClosest(List<List<Integer>> list, int K) {
+       
+		print(list);
+		Collections.sort(list, comp);
+		System.out.println("=====");
+		print(list);
+       
+        return null;
+    }
+	
+	void print(List<List<Integer>> list) {
+		for(int i=0; i<list.size();i++) {
+			List<Integer> sList =list.get(i);
+			for(int j=0; j<sList.size(); j++) {
+				Integer ii= sList.get(j);
+				System.out.println("ii: "+ii);
+			}
+		}
+	}
+	
+	Comparator<List<Integer>> comp = new Comparator<List<Integer>>() {
+
+		@Override
+		public int compare(List<Integer> a, List<Integer> b) {
+			// TODO Auto-generated method stub
+			return a.get(0)* a.get(0)+ a.get(1)*a.get(1)-
+					b.get(0)*b.get(0) + b.get(1)*b.get(1);
+		}
+		
+	};
+
+	public void print(int[][] result) {
+		for(int i=0; i< result.length; i++) {
+			for(int j=0; j< result[0].length; j++) {
+				System.out.print(result[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
+
