01fe0b8bf652e29c53a9d9468dd1f0e53ffe7497
bkjames <sweden011@gmail.com>
2019-04-05 16:44:22 +0900

    Update ZRamen_pq.java

--- 07_ka/ZRamen_pq.java
+++ 07_ka/ZRamen_pq.java
@@ -1,4 +1,4 @@
-package prog;
+package test0404;
 
 import java.util.*;
 
@@ -7,9 +7,9 @@ public class Ramen_pq {
 	public static void main(String[] args) {
 		int stock = 4;
 		int[] dates = {4,10,15};
-		int[] supplies =	 {20,5,10};
+		int[] supplies = {20,5,10};
 		int k =	 30; 
-	System.out.println(	Ramen_pq.solution(stock, dates, supplies, k));
+		System.out.println(	Ramen_pq.solution(stock, dates, supplies, k));
 		
 	}
 	  public static int solution(int stock, int[] dates, int[] supplies, int k) {
@@ -18,8 +18,9 @@ public class Ramen_pq {
 	 
 	        int index = 0;
 	        for (int i = 0; i < k; i++) {
+	        	System.out.println("i: "+i);
 	            if(index < dates.length && i == dates[index]) {
-	            	System.out.println("i: "+i+" "+ supplies[index]);
+	            	System.out.println("index:"+index+" dates.length: "+dates.length+" i: "+i+" supplies["+index+"] "+ supplies[index]+" dates["+index+"]: "+dates[index]);
 	                priorityQueue.add(supplies[index++]);
 	            }
 	            if(stock == 0) {
