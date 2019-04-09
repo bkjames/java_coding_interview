316c0f2e4cebcdf99cabbd41c466170ced4f4876
bkjames <sweden011@gmail.com>
2019-04-09 09:56:18 +0900

    Create G084_NumberOfDistinctIslands.java

---
+++ 05_G/G084_NumberOfDistinctIslands.java
@@ -0,0 +1,47 @@
+package zGoo1;
+import java.util.*;
+public class G084_NumberOfDistinctIslands {
+	
+	public static void main(String[] args) {
+		
+		int[][] grid = {
+						{1,1,0,1,1},
+						{1,0,0,0,0},
+						{0,0,0,0,1},
+						{1,1,0,1,1}
+						};
+		
+		G084_NumberOfDistinctIslands a = new G084_NumberOfDistinctIslands();
+		System.out.println(a.numDistictIsland(grid));
+		
+	}
+	
+	int numDistictIsland(int[][] grid) {
+		Set<String> set = new HashSet<>();
+		for(int i=0; i<grid.length; i++) {
+			for(int j=0; j< grid[i].length; j++) {
+				if(grid[i][j] != 0) {
+					StringBuilder sb = new StringBuilder();
+					dfs(grid, i, j, sb, "o");
+					grid[i][j]=0;
+					set.add(sb.toString());
+				}
+			}
+		}
+		return set.size();
+	}
+	void dfs(int[][] grid, int i, int j , StringBuilder sb,String dir) {
+		if(i<0||i==grid.length||j<0||j==grid[i].length||grid[i][j]==0)
+			return;
+		sb.append(dir);
+		grid[i][j] =0;
+		dfs(grid, i-1, j, sb, "u");
+		dfs(grid, i+1, j, sb, "d");
+		dfs(grid, i, j-1, sb, "l");
+		dfs(grid, i, j+1, sb, "r");
+		sb.append("b");
+		
+	}
+		
+
+}
