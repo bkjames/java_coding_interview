7625249b916c54ac7ae5691376bfb58515d8fda9
bkjames <sweden011@gmail.com>
2019-04-09 23:14:27 +0900

    Create G028_Candy.java

---
+++ 05_G/G028_Candy.java
@@ -0,0 +1,35 @@
+package zGoo1;
+
+import java.util.*;
+
+public class G028_Candy {
+
+	public static void main(String[] args) {
+		int[] ratings = {1,0,2};
+		System.out.println(candy(ratings));
+		
+	}
+	public static int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        boolean flag = true;
+        int sum = 0;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < ratings.length; i++) {
+                if (i != ratings.length - 1 && ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                    candies[i] = candies[i + 1] + 1;
+                    flag = true;
+                }
+                if (i > 0 && ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
+                    candies[i] = candies[i - 1] + 1;
+                    flag = true;
+                }
+            }
+        }
+        for (int candy : candies) {
+            sum += candy;
+        }
+        return sum;
+    }
+}
