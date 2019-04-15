12aeac36505c71a6b2af8696fd1d5aa4bdba8641
bkjames <sweden011@gmail.com>
2019-04-15 13:11:02 +0900

    Update G051_ShortestDistFromAllBuilding.java

--- 05_G/G051_ShortestDistFromAllBuilding.java
+++ 05_G/G051_ShortestDistFromAllBuilding.java
@@ -1,12 +1,15 @@
-package zGoo1;
+package zGoo001;
 import java.util.*;
-public class G051_ShortestDistFromAllBuilding {
+
+
+
+public class G051_ShortestAllBuilding {
 
 	public static void main(String[] args) {
 		int[][] grid= {{1,0,2,0,1},
-			       {0,0,0,0,0},
-			       {0,0,1,0,0}};
-		G051_ShortestDistFromAllBuilding a = new G051_ShortestDistFromAllBuilding();
+					   {0,0,0,0,0},
+				       {0,0,1,0,0}};
+		G051_ShortestAllBuilding a = new G051_ShortestAllBuilding();
 		a.shortestDistance(grid);
 
 	}
@@ -26,6 +29,7 @@ public class G051_ShortestDistFromAllBuilding {
 	                queue.offer(new int[]{i, j});
 	                bfs(queue, grid, dp, reach, n, m);
 	                countBuilding++;
+	                print(dp);
 	            }
 	        }
 	    }
@@ -42,6 +46,17 @@ public class G051_ShortestDistFromAllBuilding {
 	    }
 	    return result == Integer.MAX_VALUE ? -1 : result;
 	}
+	
+	void print(int[][] dp) {
+		System.out.println("===================================dp==");
+		for(int i=0; i<dp.length; i++) {
+			for(int j=0; j<dp[i].length;j++) {
+				System.out.print(dp[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("===================================dp==");
+	}
 
 	public void bfs(Queue<int[]> queue, int[][] grid, int[][] dp, int[][] reach, int n, int m) {
 	    int level = 1;
@@ -49,25 +64,40 @@ public class G051_ShortestDistFromAllBuilding {
 	    boolean[][] visited = new boolean[n][m];
 	    while (!queue.isEmpty()) {
 	        int size = queue.size();
+	        System.out.println("size: "+size);
+	        int[] quePeek = queue.peek();
+	        System.out.println("peek: "+quePeek[0]+" "+quePeek[1]);
 	        for (int i = 0; i < size; i++) {
 	            int[] curPoint = queue.poll();
 	            int x = curPoint[0];
 	            int y = curPoint[1];
+	            System.out.println("x: "+x+" y: "+y);
 	            for (int j = 0; j < 4; j++) {
 	                int dx = x + dir[j][0];
 	                int dy = y + dir[j][1];
+//	                System.out.println("before dx: "+dx+" dy: "+dy);
 	                if (dx < 0 || dx > n - 1 || dy < 0 || dy > m - 1 || grid[dx][dy] != 0 || visited[dx][dy]) {
 	                    continue;
 	                }
+	                System.out.println("after  dx: "+dx+" dy: "+dy);
 	                queue.offer(new int[]{dx, dy});
 	                visited[dx][dy] = true;
 	                dp[dx][dy] += level;
 	                reach[dx][dy]++;
 	            }
 	        }
+	      
 	        level++;
+	        System.out.println("========level=========: "+level);
 	    }
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
