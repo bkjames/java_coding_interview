c925bf3ad7ce1b0580b018aa1c32071367f54c38
bkjames <sweden011@gmail.com>
2019-04-17 06:25:18 +0900

    Update G009_GuessTheWord.java

--- 05_G/G009_GuessTheWord.java
+++ 05_G/G009_GuessTheWord.java
@@ -1,149 +1,128 @@
 package zGoo001;
+import java.util.*;
 
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
-
-import java.util.ArrayDeque;
-import java.util.Queue;
-
-public class G009_GuessTheWord {
-  static class Master {
-    int callCount;
-    String secretWord;
-
-    Master(String secretWord) {
-      this.secretWord = secretWord;
-    }
-
-    int guess(String word) {
-      System.out.println("Master.guess() is called " + ++callCount + " times.");
-      return getMatches(secretWord, word);
-    }
-  }
-
-  private static final int ALL_MATCHED = 6;
-
-  public void findSecretWord(String[] wordlist, Master master) {
-    int n = wordlist.length;
-    int[][] matchMatrix = new int[n][n];
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) { // TODO: j = i
-        matchMatrix[i][j] = getMatches(wordlist[i], wordlist[j]);
-      }
-    }
-    boolean[] visited = new boolean[n];
-    Queue<Integer> possibleSecretIndices = new ArrayDeque<>();
-    possibleSecretIndices.offer(0);
-    visited[0] = true;
-    while (!possibleSecretIndices.isEmpty()) {
-      int guessIndex = possibleSecretIndices.poll();
-      int matches = master.guess(wordlist[guessIndex]);
-      if (matches == ALL_MATCHED) {
-        break;
-      }
-      for (int index : matchMatrix[guessIndex]) {
-        if (!visited[index] && getMatches(wordlist[index], wordlist[guessIndex]) == matches) {
-          possibleSecretIndices.offer(index);
-          visited[index] = true;
-        }
-      }
-    }
-  }
-
-  // precondition: w1.length() == w2.length() == 6
-  private static int getMatches(String w1, String w2) {
-    int total = 0;
-    for (int i = 0; i < w1.length(); i++) {
-      if (w1.charAt(i) == w2.charAt(i)) {
-        total++;
-      }
-    }
-    return total;
-  }
-
-  public static void main(String[] args) {
-    G011_GuessTheWord solution = new G011_GuessTheWord();
-    Master master = new Master("acckzz");
-    String[] wordList = {"acckzz","ccbazz","eiowzz","abcczz"};
-    master.guess("aaaaaa");
-    solution.findSecretWord(wordList, master);
-  }
+interface Master {
+    public int guess(String word);
 }
 
-  // This is the Master's API interface.
-  // You should not implement it, or speculate about its implementation
-//  interface Master {
-//      public int guess(String word) ;
-//  }
+class MasterImpl implements Master{
+     String secret;
+
+    @Override
+    public int guess(String word) {
+        int count=0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i)==secret.charAt(i))
+                count++;
+        }
+        System.out.println("count: "+count);
+        return count;
+    }
+}
+public class G009_GuessTheWord {
+
+//	public void findSecretWord(String[] wordlist, Master master) {
+//	        int N = wordlist.length;
+//	        int[][] m = new int[N][N];
+//	        List<Integer> remain = new ArrayList<>();
+//	        for (int i = 0; i < N; i++) {
+//	            remain.add(i);
+//	            for (int j = i+1; j < N; j++) {
+//	                int c=0;
+//	                for (int k = 0; k < wordlist[i].length(); k++) {
+//	                    if(wordlist[i].charAt(k)==wordlist[j].charAt(k)) c++;
+//	                }
+//	                m[i][j]=c;
+//	                m[j][i]=c;
+//	            }
+//	        }
 //
+//	        int wordLen = wordlist[0].length();
+//	        int step=10;
+//	        Random r = new Random();
+//	        HashSet<Character> blacklist = new HashSet<>();
+//	        while(step>0){
+//	            int rand =  r.nextInt(remain.size());
+//	            int v = remain.get(rand);
+//	            remain.remove(rand);
 //
-//public class G011_GuessTheWord implements Master{
-//
-//	String  word = "acckzz";
-//	public int guess(String word) {
-//
-//		System.out.println();
-//		return 0;
-//	}
-//	public static void main(String[] args) {
-//		
-//
-//		String[] wordlist = {"acckzz","ccbazz","eiowzz","abcczz"};
-//		
-//				
-//		G011_GuessTheWord a = new G011_GuessTheWord();
-//		a.findSecretWord(wordlist, a);
-//				
-//	}
-// public void findSecretWord(String[] wordlist, Master master) {
-//		int[] count = new int[26];
-//		for (String s : wordlist) {
-//			for (char c : s.toCharArray()) {
-//				count[c - 'a']++;
-//			}
-//		}
-//		Set<String> set = new HashSet<>();
-//		for (String s : wordlist) {
-//			set.add(s);
-//		}
-//		int i = 0;
-//		while (i++ < 10) {
-//			String best = getBestCandidate(set, count);
-//			int match = master.guess(best);
-//			for (Iterator<String> ite = set.iterator(); ite.hasNext();) {
-//				if (match(best, ite.next()) != match) {
-//					// Remove the candidate that not matches.
-//					ite.remove();
-//				}
-//			}
-//		}
-//	}
-//
-//private String getBestCandidate(Set<String> wordlist, int[] count) {
-//		int max = 0;
-//		String best = "";
-//		for (String s : wordlist) {
-//			int c = 0;
-//			for (char a : s.toCharArray()) {
-//				c += count[a - 'a'];
-//			}
-//			if (c > max) {
-//				max = c;
-//				best = s;
-//			}
-//		}
-//		return best;
-//	}
-//
-//private int match(String a, String b) {
-//		int count = 0;
-//		for (int i = 0; i < a.length(); i++) {
-//			if (a.charAt(i) == b.charAt(i))
-//				count++;
-//		}
-//		return count;
-//	}
-//}
+//	            int match = master.guess(wordlist[v]);
+////	            System.out.println("match: "+match);
+//	            List<Integer> nextCandidate = new ArrayList<>();
+//	            if(match == wordLen){
+//	                nextCandidate.add(v);
+//	                remain=nextCandidate;
+//	            }else{
+//	                // 1<=match<wordLen
+//	                for(int i : remain){
+//	                    if(m[v][i]==match){
+//	                        nextCandidate.add(i);
+//	                    }
+//	                }
+//	                remain=nextCandidate;
+//	            }
+//	            step--;
+//	        }
+//	    }
+public void findSecretWord(String[] wordlist, Master master) {
+	int[] count = new int[26];
+	for (String s : wordlist) {
+		for (char c : s.toCharArray()) {
+			count[c - 'a']++;
+		}
+	}
+	Set<String> set = new HashSet<>();
+	for (String s : wordlist) {
+		set.add(s);
+	}
+	int i = 0;
+	while (i++ < 10) {
+		String best = getBestCandidate(set, count);
+		int match = master.guess(best);
+		for (Iterator<String> ite = set.iterator(); ite.hasNext();) {
+			if (match(best, ite.next()) != match) {
+				// Remove the candidate that not matches.
+				ite.remove();
+			}
+		}
+	}
+}
+
+private String getBestCandidate(Set<String> wordlist, int[] count) {
+	int max = 0;
+	String best = "";
+	for (String s : wordlist) {
+		int c = 0;
+		for (char a : s.toCharArray()) {
+			c += count[a - 'a'];
+		}
+		if (c > max) {
+			max = c;
+			best = s;
+		}
+	}
+	return best;
+}
+
+private int match(String a, String b) {
+	int count = 0;
+	for (int i = 0; i < a.length(); i++) {
+		if (a.charAt(i) == b.charAt(i))
+			count++;
+	}
+	return count;
+}
+	    public static void main(String[] args) {
+			 G009_GuessTheWord a = new G009_GuessTheWord();
+//			    Master master = new Master("acckzz");
+			    String[] wordList = {"acckzz","ccbazz","eiowzz","abcczz"};
+//			    master.guess("aaaaaa");
+//			    a.findSecretWord(wordList, master);
+			    
+			    MasterImpl master = new MasterImpl();
+			    master.secret = "eiowzz";
+			    a.findSecretWord(wordList, master);
+			    
+			  }
+}
