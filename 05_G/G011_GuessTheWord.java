80035a0ac8da5b224e938dce0b9cc03be14a14e1
bkjames <sweden011@gmail.com>
2019-04-19 06:27:03 +0900

    Update G011_GuessTheWord.java

--- 05_G/G011_GuessTheWord.java
+++ 05_G/G011_GuessTheWord.java
@@ -1,80 +1,78 @@
-package zGoo1;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+public class Solution {
+  static class Master {
+    int callCount;
+    String secretWord;
 
-  // This is the Master's API interface.
-  // You should not implement it, or speculate about its implementation
-  interface Master {
-      public int guess(String word) ;
+    Master(String secretWord) {
+      this.secretWord = secretWord;
+    }
+
+    int guess(String word) {
+      System.out.println("Master.guess() is called " + ++callCount + " times.");
+      return getMatches(secretWord, word);
+    }
   }
- 
 
-public class G009_GuessTheWord implements Master{
+  private static final int ALL_MATCHED = 6;
 
-	String  word = "acckzz";
-	public int guess(String word) {
+  public void findSecretWord(String[] wordlist, Master master) {
+    int n = wordlist.length;
+    int[][] matchMatrix = new int[n][n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) { // TODO: j = i
+        matchMatrix[i][j] = getMatches(wordlist[i], wordlist[j]);
+      }
+    }
+    boolean[] visited = new boolean[n];
+    Queue<Integer> possibleSecretIndices = new ArrayDeque<>();
+    possibleSecretIndices.offer(0);
+    visited[0] = true;
+    while (!possibleSecretIndices.isEmpty()) {
+      int guessIndex = possibleSecretIndices.poll();
+      int matches = master.guess(wordlist[guessIndex]);
+      if (matches == ALL_MATCHED) {
+        break;
+      }
+      for (int index : matchMatrix[guessIndex]) {
+        if (!visited[index] && getMatches(wordlist[index], wordlist[guessIndex]) == matches) {
+          possibleSecretIndices.offer(index);
+          visited[index] = true;
+        }
+      }
+    }
+  }
 
-		System.out.println();
-		return 0;
-	}
-	public static void main(String[] args) {
-		
+  // precondition: w1.length() == w2.length() == 6
+  private static int getMatches(String w1, String w2) {
+    int total = 0;
+    for (int i = 0; i < w1.length(); i++) {
+      if (w1.charAt(i) == w2.charAt(i)) {
+        total++;
+      }
+    }
+    return total;
+  }
 
-		String[] wordlist = {"acckzz","ccbazz","eiowzz","abcczz"};
-		
-				
-		G009_GuessTheWord a = new G009_GuessTheWord();
-		a.findSecretWord(wordlist, a);
-				
-	}
- public void findSecretWord(String[] wordlist, Master master) {
-		int[] count = new int[26];
-		for (String s : wordlist) {
-			for (char c : s.toCharArray()) {
-				count[c - 'a']++;
-			}
-		}
-		Set<String> set = new HashSet<>();
-		for (String s : wordlist) {
-			set.add(s);
-		}
-		int i = 0;
-		while (i++ < 10) {
-			String best = getBestCandidate(set, count);
-			int match = master.guess(best);
-			for (Iterator<String> ite = set.iterator(); ite.hasNext();) {
-				if (match(best, ite.next()) != match) {
-					// Remove the candidate that not matches.
-					ite.remove();
-				}
-			}
-		}
-	}
-
-private String getBestCandidate(Set<String> wordlist, int[] count) {
-		int max = 0;
-		String best = "";
-		for (String s : wordlist) {
-			int c = 0;
-			for (char a : s.toCharArray()) {
-				c += count[a - 'a'];
-			}
-			if (c > max) {
-				max = c;
-				best = s;
-			}
-		}
-		return best;
-	}
-
-private int match(String a, String b) {
-		int count = 0;
-		for (int i = 0; i < a.length(); i++) {
-			if (a.charAt(i) == b.charAt(i))
-				count++;
-		}
-		return count;
-	}
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    Master master = new Master("hbaczn");
+    String[] wordList = {
+      "gaxckt", "trlccr", "jxwhkz", "ycbfps", "peayuf", "yiejjw", "ldzccp", "nqsjoa", "qrjasy",
+      "pcldos", "acrtag", "buyeia", "ubmtpj", "drtclz", "zqderp", "snywek", "caoztp", "ibpghw",
+      "evtkhl", "bhpfla", "ymqhxk", "qkvipb", "tvmued", "rvbass", "axeasm", "qolsjg", "roswcb",
+      "vdjgxx", "bugbyv", "zipjpc", "tamszl", "osdifo", "dvxlxm", "iwmyfb", "wmnwhe", "hslnop",
+      "nkrfwn", "puvgve", "rqsqpq", "jwoswl", "tittgf", "evqsqe", "aishiv", "pmwovj", "sorbte",
+      "hbaczn", "coifed", "hrctvp", "vkytbw", "dizcxz", "arabol", "uywurk", "ppywdo", "resfls",
+      "tmoliy", "etriev", "oanvlx", "wcsnzy", "loufkw", "onnwcy", "novblw", "mtxgwe", "rgrdbt",
+      "ckolob", "kxnflb", "phonmg", "egcdab", "cykndr", "lkzobv", "ifwmwp", "jqmbib", "mypnvf",
+      "lnrgnj", "clijwa", "kiioqr", "syzebr", "rqsmhg", "sczjmz", "hsdjfp", "mjcgvm", "ajotcx",
+      "olgnfv", "mjyjxj", "wzgbmg", "lpcnbj", "yjjlwn", "blrogv", "bdplzs", "oxblph", "twejel",
+      "rupapy", "euwrrz", "apiqzu", "ydcroj", "ldvzgq", "zailgu", "xgqpsr", "wxdyho", "alrplq",
+      "brklfk"
+    };
+    solution.findSecretWord(wordList, master);
+  }
 }
