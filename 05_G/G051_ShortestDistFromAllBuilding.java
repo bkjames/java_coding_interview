f2cde110d8101a78c28b561f546a92f6a33a90e3
bkjames <sweden011@gmail.com>
2019-04-22 22:02:25 +0900

    Update G051_ShortestDistFromAllBuilding.java

--- 05_G/G051_ShortestDistFromAllBuilding.java
+++ 05_G/G051_ShortestDistFromAllBuilding.java
@@ -7,8 +7,8 @@ public class G051_ShortestAllBuilding {
 
 	public static void main(String[] args) {
 		int[][] grid= {{1,0,2,0,1},
-					   {0,0,0,0,0},
-				       {0,0,1,0,0}};
+			       {0,0,0,0,0},
+			       {0,0,1,0,0}};
 		G051_ShortestAllBuilding a = new G051_ShortestAllBuilding();
 		a.shortestDistance(grid);
 
