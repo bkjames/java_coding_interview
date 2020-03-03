503a723bd068d82457c897e602dc724c956cfe40
bkjames <sweden011@gmail.com>
2020-03-04 06:43:37 +0900

    Update ATG02_MinDominoRotationForEqualRow.java

--- 05_G/ATG02_MinDominoRotationForEqualRow.java
+++ 05_G/ATG02_MinDominoRotationForEqualRow.java
@@ -3,27 +3,23 @@ package p3;
 public class G235_MinDominoRotationForEqualRow {
 
 	public static void main(String[] args) {
-		int[] A = {2,1,2,4,2,2}, B = {5,2,6,2,3,2};
-		System.out.println(minDominoRotations(A,B));
+		int[] A = {2,1,2,4,2,2}, 
+			  B = {5,2,6,2,3,2};
+		G235_MinDominoRotationForEqualRow a = new G235_MinDominoRotationForEqualRow();
+		System.out.println(a.minDominoRotations(A, B));
 	}
-	 public static int minDominoRotations(int[] A, int[] B) {
-	        if (A.length != B.length) { return -1; }
-	        int[] countA = new int[7]; // countA[i] records the occurrence of i in A.
-	        int[] countB = new int[7]; // countB[i] records the occurrence of i in B.
-	        int[] same = new int[7]; // same[k] records the occurrence of k, where k == A[i] == B[i].
-	        for (int i = 0; i < A.length; ++i) {
-	            ++countA[A[i]];
-	            ++countB[B[i]];
-	            System.out.println(countA);
-	            if (A[i] == B[i]) { 
-	            	++same[A[i]]; 
-	            }
+	 public int minDominoRotations(int[] A, int[] B) {
+	        int[] countA = new int[7], countB = new int[7], same = new int[7];
+	        int n = A.length;
+	        for (int i = 0; i < n; ++i) {
+	            countA[A[i]]++;
+	            countB[B[i]]++;
+	            if (A[i] == B[i])
+	                same[A[i]]++;
 	        }
-	        for (int i = 1; i < 7; ++i) {
-	            if (countA[i] + countB[i] - same[i] >= A.length) {
-	                return Math.min(countA[i], countB[i]) - same[i];
-	            }
-	        }
+	        for (int i  = 1; i < 7; ++i)
+	            if (countA[i] + countB[i] - same[i] == n)
+	                return n - Math.max(countA[i], countB[i]);
 	        return -1;
 	    }
 }
