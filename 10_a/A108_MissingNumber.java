65f97ce98dffc1240ce35940eadbe87267f7122a
bkjames <sweden011@gmail.com>
2020-09-08 00:32:10 +0900

    Update A108_MissingNumber.java

--- 10_a/A108_MissingNumber.java
+++ 10_a/A108_MissingNumber.java
@@ -28,16 +28,16 @@ public class A108_MissingNumber {
 	}
 	
 //	3.Binary Search
-	public int missingNumber(int[] nums) { //binary search
-	    Arrays.sort(nums);
-	    int left = 0, right = nums.length, mid= (left + right)/2;
-	    while(left<right){
-	        mid = (left + right)/2;
-	        if(nums[mid]>mid) right = mid;
-	        else left = mid+1;
-	    }
-	    return left;
-	}
+  public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length-1;
+        while(left<=right){
+            int mid = (left + right)/2;
+            if(nums[mid]>mid) right = mid-1;
+            else left = mid+1;
+        }
+        return left;
+    }
 	
 	
 	
